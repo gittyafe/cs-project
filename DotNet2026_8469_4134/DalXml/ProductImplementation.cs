d964e46d60430c6aa19b37b951621ad9024359c5
Rivki Berenfeld <rivki3959@gmail.com>
2026-04-29 00:20:02 +0300

    do the product xml

--- DotNet2026_8469_4134/DalXml/ProductImplementation.cs
+++ DotNet2026_8469_4134/DalXml/ProductImplementation.cs
@@ -10,28 +10,81 @@ namespace Dal;
 
 internal class ProductImplementation:IProduct
 {
+    private readonly string fileSales = @"..\xml\products.xml";
+    private readonly string fileConfig = @"..\xml\data-config.xml";
+
+    private List<Product> Load()
+    {
+        List<Product> list;
+        XmlSerializer serializer = new XmlSerializer(typeof(List<Product>));
+        using (StreamReader sr = new StreamReader(fileProducts))
+        {
+            list = (List<Product>)serializer.Deserialize(sr);
+        }
+        return list.ToList();
+    }
+
+    private void Save(List<Product> list)
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(List<Product>));
+        using (StreamWriter sw = new StreamWriter(fileProducts))
+        {
+            serializer.Serialize(sw, list);
+        }
+    }
+
     public int Create(DO.Product product)
     {
-        throw new NotImplementedException();
+        List<Product> list = Load();
+        if (list.FirstOrDefault(x=>x.Id== item.Id) != null)
+        {
+            throw new DalAlreadyExistException("there is already a product with id " + item.Id);
+        }
+        XElement productConfig = XElement.Load(fileConfig);
+        int productNum = int.Parse(productConfig.Element("ProductNum").Value);
+
+        Product product = item with { Id = productNum };
+        productConfig.Element("ProductNum").SetValue(productNum+1);
+        productConfig.Save(fileConfig);
+        list.Add(product);
+        Save(list);
+        return item.Id;
     }
 
     public Product? Read(Func<Product, bool> filter)
     {
-        throw new NotImplementedException();
-    }
+        List<Product> list = Load();
+        var result = list?.FirstOrDefault(filter);
+        if ( result == null)
+        {
+            throw new DalNotExistException("there is no product with the required condition");
+        }
+        return result;    }
 
     public List<Product?> ReadAll(Func<Product, bool>? filter = null)
     {
-        throw new NotImplementedException();
+        List<Product> list = Load();
+        return list.Where(filter).ToList();  
     }
 
     public void Update(DO.Product product)
     {
-        throw new NotImplementedException();
-    }
-
+        List<Product> list = Load();
+        if (list.FirstOrDefault(x => x.Id == item.Id) == null)
+        {
+            throw new DalNotExistException("there no product with id " + item.Id);
+        }
+        var index = list.FindIndex(x => x.Id == item.Id);
+        list[index] = item;
+        Save(list);    }
+        
     void ICrud<Product>.Delete(int id)
     {
-        throw new NotImplementedException();
-    }
+        List<Product> list = Load();
+        if (list.FirstOrDefault(x => x.Id == id) == null)
+        {
+            throw new DalNotExistException("there no product with id " + id);
+        }
+        list = list.Where(x => x.Id != id).ToList();
+        Save(list);    }
 }
