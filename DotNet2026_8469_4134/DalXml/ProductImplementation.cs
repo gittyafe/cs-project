85fb37d0be4e895815dd03204a2d74b1d5ca9abf
Rivki Berenfeld <rivki3959@gmail.com>
2026-04-29 00:25:14 +0300

    fix product xml

--- DotNet2026_8469_4134/DalXml/ProductImplementation.cs
+++ DotNet2026_8469_4134/DalXml/ProductImplementation.cs
@@ -1,27 +1,28 @@
-﻿using DalApi;
+using DalApi;
 using DO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using System.IO;
 
 namespace Dal;
 
-internal class ProductImplementation:IProduct
+internal class ProductImplementation : IProduct
 {
-    private readonly string fileSales = @"..\xml\products.xml";
+    private readonly string fileProducts = @"..\xml\products.xml";
     private readonly string fileConfig = @"..\xml\data-config.xml";
 
     private List<Product> Load()
     {
-        List<Product> list;
+        if (!File.Exists(fileProducts)) return new List<Product>();
+        
         XmlSerializer serializer = new XmlSerializer(typeof(List<Product>));
         using (StreamReader sr = new StreamReader(fileProducts))
         {
-            list = (List<Product>)serializer.Deserialize(sr);
+            return (List<Product>)serializer.Deserialize(sr) ?? new List<Product>();
         }
-        return list.ToList();
     }
 
     private void Save(List<Product> list)
@@ -36,55 +37,63 @@ internal class ProductImplementation:IProduct
     public int Create(DO.Product product)
     {
         List<Product> list = Load();
-        if (list.FirstOrDefault(x=>x.Id== item.Id) != null)
-        {
-            throw new DalAlreadyExistException("there is already a product with id " + item.Id);
-        }
-        XElement productConfig = XElement.Load(fileConfig);
-        int productNum = int.Parse(productConfig.Element("ProductNum").Value);
+        
+        XElement config = XElement.Load(fileConfig);
+        int nextId = int.Parse(config.Element("ProductNum").Value);
 
-        Product product = item with { Id = productNum };
-        productConfig.Element("ProductNum").SetValue(productNum+1);
-        productConfig.Save(fileConfig);
-        list.Add(product);
+        Product newProduct = product with { Id = nextId };
+        
+        config.Element("ProductNum").SetValue(nextId + 1);
+        config.Save(fileConfig);
+
+        list.Add(newProduct);
         Save(list);
-        return item.Id;
+        return nextId;
     }
 
     public Product? Read(Func<Product, bool> filter)
     {
         List<Product> list = Load();
-        var result = list?.FirstOrDefault(filter);
-        if ( result == null)
+        var result = list.FirstOrDefault(filter);
+        if (result == null)
         {
-            throw new DalNotExistException("there is no product with the required condition");
+            throw new Exception("Product does not exist with the required condition");
         }
-        return result;    }
+        return result;
+    }
 
-    public List<Product?> ReadAll(Func<Product, bool>? filter = null)
+    public IEnumerable<Product?> ReadAll(Func<Product, bool>? filter = null)
     {
         List<Product> list = Load();
-        return list.Where(filter).ToList();  
+        if (filter == null) return list;
+        return list.Where(filter);
     }
 
     public void Update(DO.Product product)
     {
         List<Product> list = Load();
-        if (list.FirstOrDefault(x => x.Id == item.Id) == null)
-        {
-            throw new DalNotExistException("there no product with id " + item.Id);
-        }
-        var index = list.FindIndex(x => x.Id == item.Id);
-        list[index] = item;
-        Save(list);    }
+        int index = list.FindIndex(x => x.Id == product.Id);
         
-    void ICrud<Product>.Delete(int id)
+        if (index == -1)
+        {
+            throw new Exception($"Product with ID {product.Id} not found");
+        }
+        
+        list[index] = product;
+        Save(list);
+    }
+
+    public void Delete(int id)
     {
         List<Product> list = Load();
-        if (list.FirstOrDefault(x => x.Id == id) == null)
+        var product = list.FirstOrDefault(x => x.Id == id);
+        
+        if (product == null)
         {
-            throw new DalNotExistException("there no product with id " + id);
+            throw new Exception($"Product with ID {id} not found");
         }
-        list = list.Where(x => x.Id != id).ToList();
-        Save(list);    }
+        
+        list.Remove(product);
+        Save(list);
+    }
 }
