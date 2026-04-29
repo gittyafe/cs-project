ed63fefadf13680d58548d8945cef0358d77d099
Gitty <gitty7426@gmail.com>
2026-04-30 00:30:31 +0300

    open forms

---
+++ DotNet2026_8469_4134/UI/Menu.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace UI
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(345, 89);
+            button1.Name = "button1";
+            button1.Size = new Size(144, 66);
+            button1.TabIndex = 0;
+            button1.Text = "מנהל";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(345, 202);
+            button2.Name = "button2";
+            button2.Size = new Size(144, 66);
+            button2.TabIndex = 1;
+            button2.Text = "קופאי";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Menu
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Menu";
+            Text = "Menu";
+            Load += Menu_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ DotNet2026_8469_4134/UI/Menu.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Menu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            using (var manager = new Manager())
+            {
+                manager.ShowDialog();
+            }
+            this.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            using (var cashier = new Cashier())
+            {
+                cashier.ShowDialog();
+            }
+            this.Show();
+
+        }
+    }
+}
