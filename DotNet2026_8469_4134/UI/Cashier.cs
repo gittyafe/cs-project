7f7323b1e44e2641aa9c625c3b3aeae03cf7c564
RivkiBer <rivki3959@gmail.com>
2026-05-07 16:02:15 +0300

    add a cashier with a table of akk product and to do order

--- DotNet2026_8469_4134/UI/Cashier.cs
+++ DotNet2026_8469_4134/UI/Cashier.cs
@@ -16,5 +16,28 @@ namespace UI
         {
             InitializeComponent();
         }
+
+        private void buttonOKToCustID_Click(object sender, EventArgs e)
+        {
+            string custID = textBoxCustID.Text;
+            if (string.IsNullOrEmpty(custID))
+            {
+                
+                    MessageBox.Show("נא להכניס ת.ז. של הלקוח.");
+                    textBoxCustID.Focus();
+                
+
+            }
+            else
+            {
+                // Open the purchase window for the given customer ID
+                this.Hide();
+                using (var purchase = new Purchase(custID))
+                {
+                    purchase.ShowDialog();
+                }
+                this.Show();
+            }
+        }
     }
 }
